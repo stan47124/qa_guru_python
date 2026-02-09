19b2852bbbc84a3ce9cf00dfdd0c662af8f88e04
Stanila Kirill <stanila47124@gmail.com>
2026-02-09 18:45:35 +0300

    Добавлен man.py и обновлён main

--- man.py
+++ man.py
@@ -1,8 +1,6 @@
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press ⌘F8 to toggle the breakpoint.
     print(f'Hello!!!!!, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
+    # пам пам
